150b69ef7df9193fd8d192cc146cf3094d06247d
Luke Fuller <lukefuller@Lukes-MacBook-Pro.local>
2021-12-10 01:00:46 -0500

    finished postgres upload but still have bugs in code not finished

--- car_data_upload.sql
+++ car_data_upload.sql
@@ -1,29 +1,23 @@
 -- Create tables
-create table cars (
-	Vehicle_Name varchar(75) not null,
-	Full_Name varchar(150) not null,
-	Price int,
-	StockType varchar(30),
-	Distance_from_30332 float,
+create table carsData (
+	Name varchar(255) is not null,
+	Price varchar(255),
+	InventoryType varchar(75),
+	Distance varchar(255),
 	Dealership varchar(150),
-	Year varchar(75),
+	Year varchar(255),
 	Make varchar(75),
 	Model varchar(75)
 );
-
-create table ratings (
-	Vehicle_Name varchar(75) not null,
-	Rating float
+select * from carsData
+drop table audireview
+create table audiReview (
+	Name varchar(255) is not null,
+	Rating int is not null
 );
 
--- Verify imports
-select *
-from cars;
 
-select * 
-from ratings;
-
-select c.vehicle_name, c.full_name, c.price, c.stocktype, c.distance_from_30332, c. dealership, c.year, c.make, c.model, r.rating
-from cars as c
-left join ratings as r on
-c.vehicle_name = r.vehicle_name;
+select c.name, c.price, c.Inventorytype, c.Distance, c.Dealership, c.Year, c.Make, c.Model, au.Rating
+from carsdata as c
+left join audireview as au on
+c.Name = au.Name
